31f7998561246bfcecc672f21b9a54cf82189e21
Isabel Hong <hong.isabel@gmail.com>
2020-12-04 20:39:38 -0300

    edited db

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const db = () => {
     mongoose
-        .connect('mongodb://localhost:27017/Mobile', { useNewUrlParser: true })
+        .connect('mongodb://localhost:27017/Mobile', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log('DB connected');
         })
